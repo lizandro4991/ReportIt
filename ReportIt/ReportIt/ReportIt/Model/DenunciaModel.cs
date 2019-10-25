a4ed4446d580f5595eab2dadf9f303ef09cfed8d
LizandroSegovia <lizandro4991@gmail.com>
2019-10-24 20:37:46 -0600

    Commit del Moselo

---
+++ ReportIt/ReportIt/ReportIt/Model/DenunciaModel.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportIt.Model
+{
+    [Table("Tbl_Denuncias")]
+    public class DenunciaModel
+    {
+        [PrimaryKey, AutoIncrement, Unique]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string TipoViolencia { get; set; }
+        public string Direccion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
